c3a388da8fe5fbe22c16e707b9c054b2d6b3a015
Jake Kravas <jakekravas7@gmail.com>
2020-06-03 22:47:54 -0700

    Added middleware as well as functionality to get api/auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,12 +3,23 @@ const router = express.Router();
 const expressValidator = require('express-validator');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const config = require('config');
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
 
 // @route      GET api/auth
 // @desc       Test route
 // @access     Private
-router.get("/", (req, res) => {
-  res.send("Auth route");
+router.get("/", auth, async (req, res) => {
+  try {
+    // getting all user data except for password
+    const user = await User.findById(req.user.id).select("-password");
+    res.json({user});
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
 });
 
 module.exports = router;
