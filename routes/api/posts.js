69b72126c1039feb25c8a217364b89a3abcf027a
Jake Kravas <jakekravas7@gmail.com>
2020-06-05 21:29:03 -0700

    Created a route that can delete a user's specific post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -72,4 +72,33 @@ router.get("/:id", auth, async (req, res) => {
   }
 });
 
+// @route      DELETE api/posts/:id
+// @desc       Delete a post
+// @access     Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({msg: "Post not found"});
+    }
+
+    // Check user
+    if (post.user.toString() !== req.user.id){
+      return res.status(401).send({msg: "User not authorized"});
+    };
+
+    await post.remove();
+
+    res.json({ msg: "Post removed" });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId"){
+      res.status(404).send({msg: "Post not found"});
+    } else {
+      res.status(500).send("Server error");
+    }
+  }
+});
+
 module.exports = router;
