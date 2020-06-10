c6257502d60cd495ff37e6d85690a6437136b848
Jake Kravas <jakekravas7@gmail.com>
2020-06-10 15:02:56 -0700

    Added ability for users to add education and experience to their profile

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,10 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
-import CreateProfile from "./components/profile-form/CreateProfile";
-import EditProfile from "./components/profile-form/EditProfile";
+import CreateProfile from "./components/profile-forms/CreateProfile";
+import EditProfile from "./components/profile-forms/EditProfile";
+import AddExperience from "./components/profile-forms/AddExperience";
+import AddEducation from "./components/profile-forms/AddEducation";
 import PrivateRoute from "./components/routing/PrivateRoute";
 // Redux
 import { Provider } from "react-redux";
@@ -16,6 +18,7 @@ import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
 
 import './App.css';
+import { addEducation } from './actions/profile';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -40,6 +43,8 @@ const App = () => {
               <PrivateRoute exact path="/dashboard" component={Dashboard}/>
               <PrivateRoute exact path="/create-profile" component={CreateProfile}/>
               <PrivateRoute exact path="/edit-profile" component={EditProfile}/>
+              <PrivateRoute exact path="/add-experience" component={AddExperience}/>
+              <PrivateRoute exact path="/add-education" component={AddEducation}/>
             </Switch>
           </section>
         </Fragment>
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,7 +3,8 @@ import { setAlert } from "./alert";
 
 import {
   GET_PROFILE,
-  PROFILE_ERROR
+  PROFILE_ERROR,
+  UPDATE_PROFILE
 } from "./types";
 
 // Get current users profile
@@ -33,7 +34,7 @@ export const createProfile = (formData, history, edit = false) => async dispatch
     }
 
     const res = await axios.post("/api/profile", formData, config);
-    console.log(res);
+
     dispatch({
       type: GET_PROFILE,
       payload: res.data
@@ -51,6 +52,73 @@ export const createProfile = (formData, history, edit = false) => async dispatch
       errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
     }
 
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+}
+
+// Add experience
+export const addExperience = (formData, history) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  
+    const res = await axios.put("/api/profile/experience", formData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+  
+    dispatch(setAlert("Experience Added", "success"));
+  
+    history.push("/dashboard");
+  } catch (err) {
+    console.log(err);
+    const errors = err.response.data.errors; //because we named the array errors
+  
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
+  
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+}
+
+// Add education
+export const addEducation = (formData, history) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  
+    const res = await axios.put("/api/profile/education", formData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+  
+    dispatch(setAlert("Education Added", "success"));
+  
+    history.push("/dashboard");
+  } catch (err) {
+    const errors = err.response.data.errors; //because we named the array errors
+  
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
+  
     dispatch({
       type: PROFILE_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status }
