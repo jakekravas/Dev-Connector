96da8868689fcc7109306880508a09f72f69b59d
Jake Kravas <jakekravas7@gmail.com>
2020-06-13 12:15:10 -0700

    Created reducer for posts and implemented a getPosts action

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -9,3 +9,26 @@ const initialState = {
   loading: true,
   error: {}
 }
+
+export default function(state = initialState, action) {
+  const { type, payload } = action;
+  
+  switch(type){
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: payload,
+        error: false
+      }
+
+    case POST_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false
+      }
+
+    default:
+      return state;
+  }
+}
